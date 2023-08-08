3a6194a7f79045fa1abe22d44c52af15259df935
mastre <peaceyop@gmail.com>
2023-08-08 16:07:44 +0900

    Time: 160 ms (98.80%), Space: 38.9 MB (30.72%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.kt
+++ 0338-counting-bits/0338-counting-bits.kt
@@ -1,15 +1,12 @@
 class Solution {
     
     fun countBits(n: Int): IntArray {
-        val dp = Array(n + 1) { mutableListOf<Int>() }
-        dp[0] = mutableListOf(0)
+        val dp = IntArray(n + 1)
+        dp[0] = 0
         for (i in 1 until n + 1) {
-            dp[i] = dp[i - 1].apply { add(getOneCountInBinary(i)) }
+            dp[i] = dp[i / 2] + i % 2
         }
-        return dp[n].toIntArray()
+        return dp
     }
-
-    fun getOneCountInBinary(i: Int): Int {
-        return i.toString(2).count { it == '1' }
-    }
+    
 }
