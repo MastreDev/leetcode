ae4151ea15093efad2ee3eb6ed9207018be79a41
mastre <peaceyop@gmail.com>
2023-08-12 21:19:41 +0900

    Time: 164 ms (70.50%), Space: 37.5 MB (31.68%) - LeetHub

--- 0066-plus-one/0066-plus-one.kt
+++ 0066-plus-one/0066-plus-one.kt
@@ -1,20 +1,12 @@
 class Solution {
     fun plusOne(digits: IntArray): IntArray {
-        var over = 1
         for(i in digits.lastIndex downTo 0) {
-            val newVal = digits[i] + over
-            if(newVal == 10) {
-                over = 1      
-                digits[i] = 0
-            } else {
-                over = 0
-                digits[i] = newVal
+            if(digits[i] < 9) {
+                digits[i]++
+                return digits
             }
+            digits[i] = 0
         }
-        return if(over == 1) {
-            IntArray(digits.size + 1){ if(it == 0) 1 else digits[it - 1] }
-        } else {
-            digits
-        }
+        return IntArray(digits.size + 1).apply{this[0] = 1}
     }
 }
