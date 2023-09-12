e2f6fb37de5ec63bd49c1b4022200f945d5105aa
mastre <peaceyop@gmail.com>
2023-09-12 18:13:22 +0900

    Time: 388 ms (5.21%), Space: 42 MB (5.40%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.kt
@@ -0,0 +1,30 @@
+class Solution {
+    fun canPlaceFlowers(flowerbed: IntArray, n: Int): Boolean {
+        val newBed = mutableListOf<Int>()
+        val oldBed = mutableListOf<Int>().apply{this.addAll(flowerbed.toList())}
+
+        var newPlant = 0
+
+        while(oldBed.isNotEmpty()) {
+            val last = newBed.lastOrNull()
+            val current = oldBed.removeFirst()
+            val next = oldBed.firstOrNull()
+
+            if(last == 0 && current == 0 && next == 0) {
+                newBed.add(1)
+                newPlant++
+            } else if (last == null && current == 0 && next == 0){
+                newBed.add(1)
+                newPlant++
+            } else if (last == 0 && current == 0 && next == null){
+                newPlant++
+            } else if (last == null && current == 0 && next == null){
+                newPlant++
+            } else {
+                newBed.add(current)
+            }
+        }
+        
+        return newPlant >= n
+    }
+}
