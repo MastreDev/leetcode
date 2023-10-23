f262b6ebe21acac55e7714adba65d6ea09c06eae
mastre <peaceyop@gmail.com>
2023-10-23 17:56:49 +0900

    Time: 172 ms (92.19%), Space: 37.7 MB (73.10%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.kt
+++ 0036-valid-sudoku/0036-valid-sudoku.kt
@@ -1,37 +1,25 @@
 class Solution {
     fun isValidSudoku(board: Array<CharArray>): Boolean {
-        var rowValidates = true
-        var colValidates = true
-        var boxValidator = true
+        val rows = Array(9){ hashSetOf<Char>() }
+        val cols = Array(9){ hashSetOf<Char>() }
+        val boxes = Array(9){ hashSetOf<Char>() }
 
-        for(i in 0 until board.size) {
-            rowValidates = rowValidates && checkValid(board[i])
-            if(rowValidates == false) return false
-            colValidates = colValidates && board.map{ it[i] }.toCharArray().let{ checkValid(it) }
-            if(rowValidates && colValidates == false) return false
-        }
-        
-        boxWhole@ for(i in 0 until board.size step 3) {
-            for(j in 0 until board[i].size step 3) {
-                val boxValidate = hashSetOf<Char>()
-                for(k in i until i + 3) {
-                    for (l in j until j + 3) {
-                        if(board[k][l] == '.') continue
-                        if(boxValidate.add(board[k][l])) continue
-                        return false
-                        break@boxWhole
-                    }
+        for(row in 0 until 9) {
+            for(col in 0 until 9) {
+                val n = board[row][col]
+                if(n == '.') continue
+
+                val box = row / 3 * 3 + col / 3
+                if(rows[row].contains(n) || cols[col].contains(n) || boxes[box].contains(n)) {
+                    return false
                 }
+                rows[row].add(n)
+                cols[col].add(n)
+                boxes[box].add(n)
             }
         }
-        return rowValidates && colValidates && boxValidator
-    }
 
-    fun checkValid(array : CharArray) : Boolean {
-        val validator = hashSetOf<Char>()
-        for(i in array.indices) {
-            if(array[i]!= '.' && validator.add(array[i]) == false) return false
-        }
         return true
     }
+
 }
